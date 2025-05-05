c4f4231bb32efe217ab082462c1a0d73c63b91b2
Luan Feitosa Santos <luansantos26@outlook.com>
2025-05-05 20:29:43 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 console.log("Hello, World!");
 console.log(`"This is a test."`);
 var hora = 10
-console.log('A  HORA DE AGORA É ' + hora + ' horas')
+console.log('A  HORA DE AGORA É ' , hora , ' horas')
 console.log(`A HORA DE AGORA É ${hora} horas`)
