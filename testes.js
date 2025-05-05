a4704123462304bd828eef777a72a30a82d895fc
Luan Feitosa Santos <luansantos26@outlook.com>
2025-05-05 20:42:32 -0300

    comentarios

--- testes.js
+++ testes.js
@@ -1,6 +1,10 @@
 console.log("Hello, World!");
 console.log(`"This is a test."`);
 var hora = 10
-console.log('A  HORA DE AGORA É ' , hora , ' horas')
+console.log('A  HORA DE AGORA É ' ,hora, ' horas')
 console.log(`A HORA DE AGORA É ${hora} horas`)
-//comentario 
+//cometary 
+/*um cometary along
+com varias linhas. */
+
+console.log("A soma de 2 + 2 é ", 2 + 2)
