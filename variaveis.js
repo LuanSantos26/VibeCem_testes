759b1114c4e6550aea4f8ecbb84f9795eaf40138
Luan Feitosa Santos <luansantos26@outlook.com>
2025-05-05 21:38:27 -0300

    05/05/25

---
+++ variaveis.js
@@ -0,0 +1,6 @@
+let nome = "Luan";
+let idade = 25;
+let altura = 1.75;
+let estudante = true;
+let habilidades = ["JavaScript", "Python", "HTML", "CSS"];
+console.log(nome,'tem', idade, 'anos e', altura, 'de altura sua habilidades são:',habilidades);
