8d4b82fb901cb51756e48dbb61fc3379f41a9ed0
Luan Feitosa Santos <luansantos26@outlook.com>
2025-05-15 16:28:25 -0300

    var-let.js

---
+++ js/var-let.js
@@ -0,0 +1,8 @@
+let nome;
+nome ='Luan';
+console.log(nome);
+nome = "Santos"
+console.log(nome);
+
+// let nome = 'Feitosa' não podemos redeclarar variáveis com let
+// var nome = 'Luan' podemos redeclarar variáveis com var
